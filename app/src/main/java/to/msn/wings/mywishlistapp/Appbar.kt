2aaa004a37aefb70d334cef7f36c8b891e10969c
marjunstudio <uebujie@gmail.com>
2025-05-18 08:21:24 +0900

    戻るボタンの実装

--- app/src/main/java/to/msn/wings/mywishlistapp/Appbar.kt
+++ app/src/main/java/to/msn/wings/mywishlistapp/Appbar.kt
@@ -4,8 +4,13 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +19,21 @@ fun AppBarView(
     title: String,
     onBackClicked: () -> Unit = {}
 ) {
+    val navigationIcon: (@Composable () -> Unit)? =
+        if (!title.contains("WishList")) {
+            {
+                IconButton(onClick = { onBackClicked() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = null
+                    )
+                }
+            }
+        } else {
+            null
+        }
+
     TopAppBar(title = {
         Text(text = title,
             color = colorResource(id = R.color.white),
@@ -21,6 +41,7 @@ fun AppBarView(
     },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
         )
 }
 
