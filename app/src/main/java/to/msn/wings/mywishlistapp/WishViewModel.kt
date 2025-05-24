77455024720ec1352ffc1440dec114c59329d69f
marjunstudio <uebujie@gmail.com>
2025-05-24 09:31:43 +0900

    WishDAOを追加し、データアクセスの処理を実装

--- app/src/main/java/to/msn/wings/mywishlistapp/WishViewModel.kt
+++ app/src/main/java/to/msn/wings/mywishlistapp/WishViewModel.kt
@@ -4,8 +4,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import to.msn.wings.mywishlistapp.data.Wish
+import to.msn.wings.mywishlistapp.data.WishRepository
 
-class WishViewModel: ViewModel() {
+class WishViewModel(
+    private val wishRepository: WishRepository
+): ViewModel() {
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
 
@@ -15,5 +23,35 @@ class WishViewModel: ViewModel() {
 
     fun onWishDescriptionChanged(newString: String) {
         wishDescriptionState = newString
+    }
+
+    lateinit var getAllWishes: Flow<List<Wish>>
+
+    init {
+        viewModelScope.launch {
+            getAllWishes = wishRepository.getWishes()
+        }
+    }
+
+    fun addWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.addAWish(wish)
+        }
+    }
+
+    fun getAWishById(id: Long):Flow<Wish> {
+        return wishRepository.getAWishById(id)
+    }
+
+    fun updateWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.updateAWish(wish)
+        }
+    }
+
+    fun deleteWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.deleteAWish(wish)
+        }
     }
 }
